27eefa0ab0470e5e2bb51ed5c0f87aeafefeb251
Ismail <isse_a@hotmail.com>
2025-12-30 19:06:50 +0100

    Initial Commit

---
+++ SpaceTests/SpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTests.swift
+//  SpaceTests
+//
+//  Created by Ismail Mohammed on 2025-12-30.
+//
+
+import Testing
+@testable import Space
+
+struct SpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
